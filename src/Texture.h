d6988d39f4b6b824e7424dffc46f46e497b83b1e
UnknownRedFoxDev <unknown.redfoxo@gmail.com>
2025-07-24 18:57:00 +0200

    forgot to do the inline getter methods

--- src/Texture.h
+++ src/Texture.h
@@ -13,4 +13,7 @@ class Texture {
 
 		 void Bind(unsigned int slot = 0) const;
 		 void Unbind() const;
+
+		 inline int GetWidth() const { return m_Width; }
+		 inline int GetHeight() const { return m_Height; }
 };
