c52e821d90a5d150155fb219404cb47fb6205748
UnknownRedFoxDev <unknown.redfoxo@gmail.com>
2025-07-24 18:54:56 +0200

    feat: Added Texture class and its base component + methods (raw)

--- src/Texture.cpp
+++ src/Texture.cpp
@@ -1,1 +1,13 @@
 #include "Texture.h"
+
+Texture::Texture(const std::string& path) : m_Renderer(0), m_FilePath(path), m_DataBuffer(0), m_Height(0), m_Width(0) {
+}
+
+Texture::~Texture(){
+}
+
+void Texture::Bind(unsigned int slot) const {
+}
+
+void Texture::Unbind() const {
+}
--- src/Texture.h
+++ src/Texture.h
@@ -1,1 +1,16 @@
 #pragma once
+
+ #include <string>
+class Texture {
+	 private:
+		 unsigned int m_Renderer;
+		 int m_Width, m_Height;
+		 std::string m_FilePath;
+		 unsigned char* m_DataBuffer;
+	public:
+		 Texture(const std::string& path);
+		 ~Texture();
+
+		 void Bind(unsigned int slot = 0) const;
+		 void Unbind() const;
+};
