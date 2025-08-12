9add9ca418191ceebc63a84d767175da148c5aa4
UnknownRedFoxDev <unknown.redfoxo@gmail.com>
2025-08-12 17:08:11 +0200

    Removed the extra context and comments

--- src/tests/TestBatchRenderingTextures.cpp
+++ src/tests/TestBatchRenderingTextures.cpp
@@ -123,43 +123,22 @@ namespace test {
 			0, 1, 2,
 			2, 3, 0
 		};
-		// float vertices[] = {
-		// 	-0.5f, -0.5f, 0.0f, 0.0f,
-		// 	 0.5f, -0.5f, 1.0f, 0.0f,
-		// 	 0.5f,  0.5f, 1.0f, 1.0f,
-		// 	-0.5f,  0.5f, 0.0f, 1.0f,
-		// };
-		//
-		// uint indices[] = {
-		// 	0, 1, 2,
-		// 	2, 3, 0
-		// };
-		// m_EBO = std::make_unique<IndexBuffer>(indices, 6);
-		// m_VBO = std::make_unique<VertexBuffer>(vertices, 4 * 4 * sizeof(float));
+
 		m_Layout.Push<float>(2);
 		m_Layout.Push<float>(2);
-		//
-		// m_VAO = std::make_unique<VertexArray>();
+
 		m_Shader = std::make_shared<Shader>(vertPath, fragPath);
-		// m_VAO->AddBuffer(*m_VBO, m_Layout);
-		// m_Shader->Bind();
 		m_ObjInfo = std::make_unique<Geometry>(vertices, indices, m_Layout, m_Shader);
 
 		m_Texture = std::make_shared<Texture>(defaultTexPath);
-		// m_Shader->Bind();
-		// m_Shader->SetUniform1i("uTexture", 0);
 	}
 
 	void BackendTest::Render(glm::mat4 mvp)
 	{
 		Renderer renderer;
-		// m_Texture->Bind();
-
-		{
-			m_Shader->Bind();
-			m_Shader->SetUniformMat4f("u_MVP", mvp);
-			renderer.Draw(*m_ObjInfo->m_VAO, *m_ObjInfo->m_EBO, *m_Shader);
-		}
+		m_Shader->Bind();
+		m_Shader->SetUniformMat4f("u_MVP", mvp);
+		renderer.Draw(*m_ObjInfo->m_VAO, *m_ObjInfo->m_EBO, *m_Shader);
 	}
 }
 
